d4f1eb7b381eebfb18b333bddd09001dd179f977
Tim Zhamoidin <timofeyzhamoidin@gmail.com>
2024-10-17 15:16:54 +0300

    my small changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -48,6 +48,9 @@ keymap.set("n", [[\d]], "<cmd>bprevious <bar> bdelete #<cr>", {
   desc = "delete buffer",
 })
 
+-- Clangd switch source and header
+vim.api.nvim_set_keymap('n', '<leader>ss', ':ClangdSwitchSourceHeader<CR>', { noremap = true, silent = true })
+
 -- Insert a blank line below or above current line (do not move the cursor),
 -- see https://stackoverflow.com/a/16136133/6064933
 keymap.set("n", "<space>o", "printf('m`%so<ESC>``', v:count1)", {
